04c453e707ac1e2edc13b8b4777cf64ca5ae6d8c
Bachirdiallo <moubachir03@gmail.com>
2015-12-23 19:41:08 +0800

    Create user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
+  devise_for :users, controllers: {registrations: 'registrations'}
+
+  authenticated :user do
+    devise_scope :user do
+      #root to: "users#dashboard"
+      get '/users/sign_out' => 'devise/sessions#destroy'
+    end
+  end
+
+  unauthenticated do
+  devise_scope :user do
+    root to: "devise/sessions#new", :as => "unauthenticated"
+  end
+end
 end
